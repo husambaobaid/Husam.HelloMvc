60ae3247ceb882f3ab74c4c9a166b2c3c2b922f8
husambaobaid <hussambaobeid35@gmail.com>
2025-08-31 00:07:19 +0800

    Milestone 2: routing + basic form + validation

---
+++ Husam.HelloMvc/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Husam.HelloMvc.Models;
+
+namespace Husam.HelloMvc.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /Hello/Greet
+        [HttpGet]
+        public IActionResult Greet()
+        {
+            return View(new GreetViewModel());
+        }
+
+        // POST: /Hello/Greet
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Greet(GreetViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            model.Message = $"Hello, {model.Name}!";
+            return View(model);
+        }
+
+        // (Optional) Your previous Index action can stay here too.
+    }
+}
---
+++ Husam.HelloMvc/Models/GreetViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Husam.HelloMvc.Models
+{
+    public class GreetViewModel
+    {
+        [Display(Name = "Your name")]
+        [Required, StringLength(40)]
+        public string? Name { get; set; }
+
+        // We’ll fill this after POST to show the greeting
+        public string? Message { get; set; }
+    }
+}
